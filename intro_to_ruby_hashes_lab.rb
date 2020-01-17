3f3a70daafb6b2b9f3ac22b0b36b25e3b4d1febc
Hannah Drury <hannah_drury@yahoo.com>
2020-01-17 23:50:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  hash = {key => value}
 end
 
 def read_from_hash(hash, key)
