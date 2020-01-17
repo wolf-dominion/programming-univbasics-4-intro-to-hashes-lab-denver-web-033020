d130aebee1a6ce77a0852908e9a77ecbb869ee82
Hannah Drury <hannah_drury@yahoo.com>
2020-01-17 23:57:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key] = nil
     return hash[key] => 1
-  end
   else
     hash[key] += 1
+  end
 end
