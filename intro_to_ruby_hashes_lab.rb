c3dc1824bd0f2dac074699d575af77ced09357f7
Hannah Drury <hannah_drury@yahoo.com>
2020-01-17 23:48:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,11 +4,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  hash= {key: "value"}
+  hash = {key: "value"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {key: "Grace Hopper"
 end
 
 def id_generator
